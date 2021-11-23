e335f14d7b04557e59234e20e1e2e0b0cff7370c
Number-One-Truth <70406359+Number-One-Truth@users.noreply.github.com>
2021-11-23 18:32:28 -0500

    Delete newscript.js

--- newscript.js
+++
@@ -1,7 +0,0 @@
-const icon = document.querySelector(".hamburger-menu");
-const slidenavbar = document.querySelector(".slidenavbar");
-
-
-icon.addEventListener("click", () => {
-        slidenavbar.classList.toggle("change");
-    });
